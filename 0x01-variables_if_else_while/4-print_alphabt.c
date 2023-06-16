9a7a56a7664ea16b29548cb36f6038333fcd2bc8
sharon mutisya <sharonmutisya81@gmail.com>
2023-06-16 08:33:48 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,11 +12,11 @@ int main(void)
 	e = 'e';
 	q = 'q';
 
-	for (low ='a'; low <='z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 	{
-		if (low !=e && low !=q)
+		if (low != e && low != q)
 			putchar(low);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
