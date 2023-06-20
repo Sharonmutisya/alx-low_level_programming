524afdecc130e09765ddd887951bd48eca28df64
sharon mutisya <sharonmutisya81@gmail.com>
2023-06-20 05:05:46 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char C = 'a';
+
+	while (C <= 'z')
+	{
+		_putchar(C);
+		C++;
+	}
+	_putchar('\n');
+}
