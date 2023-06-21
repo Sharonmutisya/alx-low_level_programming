e7be18ec04251a9a783dff053622a329d4371718
sharon mutisya <sharonmutisya81@gmail.com>
2023-06-21 03:16:17 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
- * print_alphabet_*10 - Print lowercase alphabet 10 times
+ * print_alphabet_x10 - Print lowercase alphabet 10 times
  */
-void print_alphabet_*10(void)
+
+void print_alphabet_x10(void)
 {
 	char num, letter;
 
