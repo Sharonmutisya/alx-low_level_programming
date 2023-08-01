9fa7a71b16be1e25b3f450094b749b969dde2193
Sharonmutisya <sharonmutisya81@gmail.com>
2023-08-01 04:25:58 -0700

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns node at a certain index.
+ * @head: first node.
+ * @index: index of the node to return.
+ * Return: Pointer to the node we are looking for, or NULL.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp = head;
+
+	while (temp && i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+	return (temp ? temp : NULL);
+}
