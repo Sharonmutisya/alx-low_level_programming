09149ce1c394bbc9fe51d4766a0709ab95d45ac9
Sharonmutisya <sharonmutisya81@gmail.com>
2023-08-01 09:19:30 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * pop_intlist - Deletes the head node.
+ * pop_listint - Deletes the head node.
  * @head: pointer to the first element.
  * Return: Data inside the elements deleted,
- * or 0 if the list is empty.
+ * or 0 if list is empty.
  */
-int pop_intlist(listint_t **head)
+int pop_listint(listint_t **head)
 {
 	listint_t *temp;
 	int num;
